64d539590da75c5cdbf909d158945a92c8983ec5
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-12 10:24:09 +0200

    Comment out summertime

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,5 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all().filter(status=1)
+    queryset = Post.objects.all()
     template_name = "post_list.html"
