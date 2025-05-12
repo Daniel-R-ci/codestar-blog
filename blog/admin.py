7b1eaff6684045514ecd6a723ec8786d68aa117b
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-12 10:23:57 +0200

    Comment out Summertime

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+"""
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status')
@@ -9,9 +10,11 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title', )}
     summernote_fields = ('content',)
+"""
 
 
 # Register your models here.
 
 
+admin.site.register(Post)
 admin.site.register(Comment)
