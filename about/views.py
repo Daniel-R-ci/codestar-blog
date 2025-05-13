aae5db97f61b2dcfa824e1c94054d68211150d7c
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-13 15:06:37 +0200

    Add About app

---
+++ about/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+
+def about_me(request):
+    queryset = About.objects.all().order_by("-updated_on")
+    about = queryset.first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+# Register your models here.
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
@@ -10,9 +12,6 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title', )}
     summernote_fields = ('content',)
-
-
-# Register your models here.
 
 
 admin.site.register(Comment)
