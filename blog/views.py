6678e2adcd594edb0d691ec110c697ac66732865
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-13 13:30:27 +0200

    Finish view lesson

--- blog/views.py
+++ blog/views.py
@@ -6,21 +6,22 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     # template_name = "post_list.html"
     template_name = "blog/index.html"
     paginate_by = 6
 
+
 def post_detail(request, slug):
     """
     Display and individual :model: blog.Poost
- 
+
     **Context**
     ``post``
      An instance of :model: `blog.Post`.
- 
+
     **Template:**
-    
+
     :template: `blog/post_detail.html`
     """
 
